00ef84d9941d0a759055f4740b40e1b768693706
Alexey Maksutov <alexey.maksutov@gmail.com>
2026-04-30 15:58:18 -0400

    updates

--- etc/entrypoint.sh
+++ etc/entrypoint.sh
@@ -2,6 +2,5 @@
 
 [ -e /etc/profile ] || (set -- /etc/entrypoint/*.sh && [ -e "$1" ] && for FILE; do . "${FILE}"; done; export -p > /etc/profile) || exit $?
 [ -e /etc/profile ] || exit 127
-
 . /etc/profile
 exec "$@"
